10196b446c6a11e1ed204c100c06a5252927978c
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2018-10-10 11:51:02 +0100

    uploaded assets folder
    assets from Wednesday 10/10/2018th's  session 
    
    scripts, images and the scene

---
+++ Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour {
+	public float forwardSpeed = 20;
+	public float turnSpeed = 150;
+	[Range(0,1)]
+	public float drift = 0.9f;
+	public float maxGripSpeed = 2.5f;
+	Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float forwardInput = Input.GetAxis ("Vertical");
+		float turnInput = Input.GetAxis ("Horizontal");
+		float driftAmount = 1;
+		if (RightVelocity().magnitude > maxGripSpeed) 
+		{
+			driftAmount = drift;
+		}
+		body.velocity = ForwardVelocity() + RightVelocity() * driftAmount;
+
+		if (forwardInput > 0) 
+		{
+			body.AddForce (transform.up * forwardSpeed * forwardInput);
+		}
+		float tf = Mathf.Lerp (0, -turnSpeed, body.velocity.magnitude * 0.5f);
+		body.angularVelocity = turnInput * tf;
+	}
+	Vector2 ForwardVelocity()
+	{
+		return transform.up * Vector2.Dot(body.velocity, transform.up);
+	}
+	Vector2 RightVelocity() {
+		return transform.right * Vector2.Dot(body.velocity, transform.right);
+	}
+}
